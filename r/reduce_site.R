7b58414de11f4bd4fd942586848ebd4f6ebe1705
JinyanYang <jyyoung@live.com>
2022-03-09 14:16:59 +1100

    data

--- r/reduce_site.R
+++ r/reduce_site.R
@@ -1,20 +1,30 @@
 library(ncdf4)
 
-met.fn.vec <- list.files('download/flux_anna/',full.names = T)
+met.fn.vec <- list.files('download/flux_anna/',full.names = T,include.dirs = FALSE)
 
 siteVpdHigh.fn <- c()
 
 for (i in seq_along(met.fn.vec)) {
-  nc.tmp <- nc_open(met.fn.vec[i])
-  vpd.vec <- ncvar_get(nc.tmp,'VPD')
+  nc.tmp <- try(nc_open(met.fn.vec[i]))
   
-  vpd.high <- vpd.vec[vpd.vec>25]
-  
-  if(length(vpd.high)>100){
-    siteVpdHigh.fn[i] <- met.fn.vec[i]
-  }else{
-    siteVpdHigh.fn[i] <- NA
+  if(class(nc.tmp)!='try-error'){
+    vpd.vec <- ncvar_get(nc.tmp,'VPD')
+    if(max(vpd.vec,na.rm=T)>10){
+      vpd.high <- vpd.vec[vpd.vec>30]
+    }else{
+      vpd.high <- vpd.vec[vpd.vec>3]
+    }
+    
+    
+    if(length(vpd.high)>1000){
+      siteVpdHigh.fn[i] <- met.fn.vec[i]
+    }else{
+      siteVpdHigh.fn[i] <- NA
+    }
   }
+  
+  
+
   
 }
 siteVpdHigh.fn <- siteVpdHigh.fn[!is.na(siteVpdHigh.fn)]
